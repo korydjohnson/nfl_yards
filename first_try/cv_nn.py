8071d5818fe89bb426102b0d3584954ba47496b3
danijel <d.kivaranovic@gmail.com>
2019-10-20 23:15:10 +0200

    updated network and nn_cv

--- first_try/cv_nn.py
+++ first_try/cv_nn.py
@@ -5,11 +5,13 @@ from sklearn.preprocessing import StandardScaler
 
 import torch
 from network.network import nn1
+from network.network import Preprocessor
 
 from scripts.clean_data import DataCleaner
 from scripts.make_features import FeatureGenerator
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+#device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cpu')
 
 print('- read data')
 df = pd.read_csv('./input/train.csv', low_memory=False)
@@ -23,7 +25,7 @@ observed = []
 predicted = []
 playids = []
 nfolds = 5
-for i in range(nfolds):
+for i in range(1,nfolds):
     print('- Fold %d' %(i+1))
 
     intr = np.where(df['fold']!=i)[0]
@@ -39,52 +41,46 @@ for i in range(nfolds):
 
     print('  - get features')
     featgenerator = FeatureGenerator(df_tr)
-    xtr, ytr, playid_tr = featgenerator.make_features(df_tr)
-    xte, yte, playid_te = featgenerator.make_features(df_te)
+    xtr, ytr, playid_tr = featgenerator.make_features(df_tr, is_train=True)
+    xte, yte, playid_te = featgenerator.make_features(df_te, is_train=True)
 
-    print('  - initialize network')
+    print('  - transform features')
     feat_num = ['YardLine', 'Distance', 'DefendersInTheBox', 'Week']
     feat_cat = [
         'Quarter', 'PossessionTeam', 'Down',
         'OffenseFormation', 'OffensePersonnel', 'DefensePersonnel',
         'HomeTeamAbbr', 'VisitorTeamAbbr', 'StadiumType', 'Turf', 'GameWeather',
     ]
+    preproc = Preprocessor(xtr, ytr, feat_num, feat_cat)
+    xtr, ytr = preproc.transform_data(xtr, ytr, is_train=True)
+    xte, yte = preproc.transform_data(xte, yte, is_train=True)
+    n_unique = xtr[1].max(axis=0).values.detach().numpy() + 1
 
-    scaler = StandardScaler()
-    scaler.fit(xtr[feat_num])
-    xtr[feat_num] = scaler.transform(xtr[feat_num])
-    xte[feat_num] = scaler.transform(xte[feat_num])
-
-    xtr_num = torch.from_numpy(xtr[feat_num].values).float().to(device)
-    xte_num = torch.from_numpy(xte[feat_num].values).float().to(device)
-
-    n_unique = xtr[feat_cat].max().values + 1
-    xtr_cat = torch.from_numpy(xtr[feat_cat].values).long().to(device)
-    xte_cat = torch.from_numpy(xte[feat_cat].values).long().to(device)
-
-    ytr = pd.get_dummies(pd.Categorical(ytr.iloc[:,0] + 99, range(199))).cumsum(axis=1).values.astype(float)
-    yte = pd.get_dummies(pd.Categorical(yte.iloc[:,0] + 99, range(199))).cumsum(axis=1).values.astype(float)
-    ytr = torch.from_numpy(ytr).float().to(device)
-    yte = torch.from_numpy(yte).float().to(device)
-
-    xtr = [xtr_num, xtr_cat]
-    xte = [xte_num, xte_cat]
-
-    input_size = len(feat_num) + 2 * len(feat_cat)
-    net = nn1(input_size=input_size, hidden_size=25, output_size=199, batch_size=256, n_unique=n_unique).to(device)
-
+    # input_size = len(feat_num) + 2 * len(feat_cat)
+    # net = nn1(input_size=input_size, hidden_size=25, output_size=199, batch_size=256, n_unique=n_unique).to(device)
+    #
     # for j in range(100):
     #     net.train_epochs(nepochs=1, X=xtr, y=ytr)
     #     loss = net.eval_model(xte, yte)
     #     print('  - iter %d - loss=%.5f' % (j+1, loss))
 
     print('  - train network')
-    net.train_epochs(nepochs=50, X=xtr, y=ytr)
-    loss = net.eval_model(xte, yte)
+    nbags = 3
+    input_size = len(feat_num) + 2 * len(feat_cat)
+    preds = np.zeros(yte.shape)
+
+    for j in range(nbags):
+        print('  - bag %d' %(j+1))
+        net = nn1(input_size=input_size, hidden_size=25, output_size=199, batch_size=256, n_unique=n_unique).to(device)
+        net.train_epochs(nepochs=40, X=xtr, y=ytr)
+        preds += net.predict(xte).cpu().detach().numpy()
+
+    preds /= nbags
+    loss = np.mean((yte.cpu().detach().numpy()-preds)**2)
     print('  - CRPS=%.5f' % (loss))
 
     observed.append(yte.cpu().detach().numpy())
-    predicted.append(net.predict(xte).cpu().detach().numpy())
+    predicted.append(preds)
     playids.append(playid_te)
 
 
@@ -98,3 +94,52 @@ print('- CRPS=%.5f' %(loss))
 
 print('Done!!!')
 
+# - read data
+# - merge cv folds to train data
+# - Fold 1
+#   - clean data
+#   - get features
+#   - initialize network
+#   - train network
+#   - bag 1
+#   - bag 2
+#   - bag 3
+#   - CRPS=0.01414
+# - Fold 2
+#   - clean data
+#   - get features
+#   - initialize network
+#   - train network
+#   - bag 1
+#   - bag 2
+#   - bag 3
+#   - CRPS=0.01459
+# - Fold 3
+#   - clean data
+#   - get features
+#   - initialize network
+#   - train network
+#   - bag 1
+#   - bag 2
+#   - bag 3
+#   - CRPS=0.01353
+# - Fold 4
+#   - clean data
+#   - get features
+#   - initialize network
+#   - train network
+#   - bag 1
+#   - bag 2
+#   - bag 3
+#   - CRPS=0.01391
+# - Fold 5
+#   - clean data
+#   - get features
+#   - initialize network
+#   - train network
+#   - bag 1
+#   - bag 2
+#   - bag 3
+#   - CRPS=0.01388
+# - compute overall CV score
+# - CRPS=0.01401
