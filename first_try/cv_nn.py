7098cb546301fbf06478cb01b1326a339942e6bc
Danijel Kivaranovic <d.kivaranovic@gmail.com>
2019-10-20 16:53:27 +0200

    cv with neural network

--- first_try/cv_nn.py
+++ first_try/cv_nn.py
@@ -6,8 +6,8 @@ from sklearn.preprocessing import StandardScaler
 import torch
 from network.network import nn1
 
-from eda_Py.clean_data import DataCleaner
-from eda_Py.make_features import FeatureGenerator
+from scripts.clean_data import DataCleaner
+from scripts.make_features import FeatureGenerator
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -24,7 +24,7 @@ predicted = []
 playids = []
 nfolds = 5
 for i in range(nfolds):
-    print('- Fold %d' %(i))
+    print('- Fold %d' %(i+1))
 
     intr = np.where(df['fold']!=i)[0]
     inte = np.where(df['fold']==i)[0]
@@ -38,23 +38,63 @@ for i in range(nfolds):
     df_te = cleaner.clean_data(df_te)
 
     print('  - get features')
-    #featgenerator = FeatureGenerator(df_tr)
-    #xtr, ytr, playid_tr = featgenerator.make_features(df_tr)
-    #xte, yte playid_te = featgenerator.make_features(df_te)
+    featgenerator = FeatureGenerator(df_tr)
+    xtr, ytr, playid_tr = featgenerator.make_features(df_tr)
+    xte, yte, playid_te = featgenerator.make_features(df_te)
 
     print('  - initialize network')
-    #input_size = len(feat_num) + 2 * len(feat_cat)
-    #net = nn1(input_size=input_size, hidden_size=25, output_size=199, batch_size=256, n_unique=n_unique).to(device)
+    feat_num = ['YardLine', 'Distance', 'DefendersInTheBox', 'Week']
+    feat_cat = [
+        'Quarter', 'PossessionTeam', 'Down',
+        'OffenseFormation', 'OffensePersonnel', 'DefensePersonnel',
+        'HomeTeamAbbr', 'VisitorTeamAbbr', 'StadiumType', 'Turf', 'GameWeather',
+    ]
+
+    scaler = StandardScaler()
+    scaler.fit(xtr[feat_num])
+    xtr[feat_num] = scaler.transform(xtr[feat_num])
+    xte[feat_num] = scaler.transform(xte[feat_num])
+
+    xtr_num = torch.from_numpy(xtr[feat_num].values).float().to(device)
+    xte_num = torch.from_numpy(xte[feat_num].values).float().to(device)
+
+    n_unique = xtr[feat_cat].max().values + 1
+    xtr_cat = torch.from_numpy(xtr[feat_cat].values).long().to(device)
+    xte_cat = torch.from_numpy(xte[feat_cat].values).long().to(device)
+
+    ytr = pd.get_dummies(pd.Categorical(ytr.iloc[:,0] + 99, range(199))).cumsum(axis=1).values.astype(float)
+    yte = pd.get_dummies(pd.Categorical(yte.iloc[:,0] + 99, range(199))).cumsum(axis=1).values.astype(float)
+    ytr = torch.from_numpy(ytr).float().to(device)
+    yte = torch.from_numpy(yte).float().to(device)
+
+    xtr = [xtr_num, xtr_cat]
+    xte = [xte_num, xte_cat]
+
+    input_size = len(feat_num) + 2 * len(feat_cat)
+    net = nn1(input_size=input_size, hidden_size=25, output_size=199, batch_size=256, n_unique=n_unique).to(device)
+
+    # for j in range(100):
+    #     net.train_epochs(nepochs=1, X=xtr, y=ytr)
+    #     loss = net.eval_model(xte, yte)
+    #     print('  - iter %d - loss=%.5f' % (j+1, loss))
 
     print('  - train network')
-    #net.train_epochs(nepochs=100, X=xtr, y=ytr)
-    #loss = net.eval_model(xte, yte)
-    #print('  - iter %d - loss=%.f' % (i, loss))
+    net.train_epochs(nepochs=50, X=xtr, y=ytr)
+    loss = net.eval_model(xte, yte)
+    print('  - CRPS=%.5f' % (loss))
 
-    #observed.append(yte)
-    #predicted.append(net.predict(xte))
-    #playids.append(playid_te)
+    observed.append(yte.cpu().detach().numpy())
+    predicted.append(net.predict(xte).cpu().detach().numpy())
+    playids.append(playid_te)
 
-score = np.mean((y-pred)**2)
-print('-  CV-Score=%.4f' %(score))
+
+print('- compute overall CV score')
+observed = np.concatenate(observed)
+predicted = np.concatenate(predicted)
+playids = np.concatenate(playids)
+
+loss = np.mean((observed-predicted)**2)
+print('- CRPS=%.5f' %(loss))
+
+print('Done!!!')
 
